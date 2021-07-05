1cc50ee502e4149ac40cdaa56611128152ae7437
rajAnkit1 <iceycoolankitraj@gmail.com>
2021-07-05 13:25:52 +0530

    this is the commit from IST time zone

--- src/test/java/ankTest/ApiTest.java
+++ src/test/java/ankTest/ApiTest.java
@@ -8,12 +8,16 @@ public class ApiTest {
 	public void soap()
 	{
 		System.out.println("soap api automation");
+		System.out.println("api automation through xml");
+		
 	}
 	
 	@Test
 	public void rest()
 	{
 		System.out.println("Restfull api automation");
+		System.out.println("Api automation through postman");
+		System.out.println("reporting bug using jira");
 	}
 
 }
